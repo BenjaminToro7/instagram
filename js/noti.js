99af745198e3027ea6c165240ed144ff6bad01dc
SYNC <aibarragonzalez2008@gmail.com>
2026-06-15 08:26:11 -0400

    update noti js

--- js/noti.js
+++ js/noti.js
@@ -1,7 +1,1 @@
-// BOTON CERRAR
-document.getElementById("closeBtn").onclick = () => alert("Cerrado");
 
-// BOTONES SEGUIR
-document.querySelectorAll(".follow-btn").forEach(btn => {
-  btn.onclick = function() { this.textContent = "Siguiendo"; this.classList.add("following"); };
-});
